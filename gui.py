83ba6ac1a1a634504b5eaf6f03dbf44fdf072e0f
lubinets <lubineckarina@gmail.com>
2024-06-03 17:01:21 +0200

    Add clock, change theme, handle case errors

--- gui.py
+++ gui.py
@@ -1,55 +1,69 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme('DarkPurple7')
+
+clock = sg.Text('', key='clock')
 label = sg.Text('Type in a todo')
 input_box = sg.InputText(tooltip='Enter a todo', key='todo')
 add_button = sg.Button('Add')
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=[45,10])
+                      enable_events=True, size=(45, 10))
 edit_button = sg.Button('Edit')
 complete_button = sg.Button('Complete')
 exit_button = sg.Button('Exit')
 
 window = sg.Window('ToDo App',
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=('Helvetica', 20))
 while True:
-    event, values = window.read()
-    # print(event)
-    # print(values)
-    # print(values['todos'])
-    match event:
-        case 'Add':
-            todos = functions.get_todos()
-            new_todo = values['todo'] + '\n'
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-        case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + '\n'
+    event, values = window.read(timeout=1000)
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-        case 'Complete':
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
-        case 'Exit':
-            break
-        case 'todos':
-            window['todo'].update(value=values['todos'][0])
-        case sg.WIN_CLOSED:
-            break
+    if event in (None, 'Exit', sg.WIN_CLOSED):
+        break
+
+    if values is not None:
+        window['clock'].update(value=time.strftime('%b %d, %Y %H:%M:%S'))
+        match event:
+            case 'Add':
+                todos = functions.get_todos()
+                if values['todo'] != '':
+                    new_todo = values['todo'] + '\n'
+                    todos.append(new_todo)
+                    functions.write_todos(todos)
+                    window['todos'].update(values=todos)
+                    window['todo'].update(value='')
+                else:
+                    sg.popup('Please enter a todo first', title='Error', font=('Helvetica', 16))
+            case 'Edit':
+                try:
+                    todo_to_edit = values['todos'][0]
+                    new_todo = values['todo'] + '\n'
+
+                    todos = functions.get_todos()
+                    index = todos.index(todo_to_edit)
+                    todos[index] = new_todo
+                    functions.write_todos(todos)
+                    window['todos'].update(values=todos)
+                    window['todo'].update(value='')
+                except IndexError:
+                    sg.popup('Please select an item first', title='Error', font=('Helvetica', 16))
+            case 'Complete':
+                try:
+                    todo_to_complete = values['todos'][0]
+                    todos = functions.get_todos()
+                    todos.remove(todo_to_complete)
+                    functions.write_todos(todos)
+                    window['todos'].update(values=todos)
+                    window['todo'].update(value='')
+                except IndexError:
+                    sg.popup('Please select an item first', title='Error', font=('Helvetica', 16))
+            case 'todos':
+                window['todo'].update(value=values['todos'][0])
 
 window.close()
-
